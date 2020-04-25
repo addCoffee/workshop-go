6dc7f18f8732e297b3171afe010ee738acfb83d6
kkapyrin <kkapyrin@phoenixit.ru>
2020-04-25 12:00:10 +0300

    First commit. Handler, chi.

---
+++ cmd/workshop/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"workshop/internal/handler"
+
+	"github.com/go-chi/chi"
+)
+
+func main() {
+
+	h := handler.NewHandler()
+
+	r := chi.NewRouter()
+
+	r.Get("/hello", h.Hello)
+
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
+
+	log.Print("shuttin server down")
+}
