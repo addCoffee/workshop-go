bbfc7cce5ed2a0bb96b352070233cfa915e3bc2f
kkapyrin <kkapyrin@phoenixit.ru>
2020-04-25 13:03:17 +0300

    Iprovements and fix config usage

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -26,7 +26,11 @@ func main() {
 
 	r.Get("/hello", h.Hello)
 
-	err = http.ListenAndServe(":8080", r)
+	path := cfg.Host + ":" + cfg.Port
+
+	log.Printf("starting server at %s", path)
+	err = http.ListenAndServe(path, r)
+
 	log.Fatal(err)
 
 	log.Print("shuttin server down")
